133b52e3d595d91e5fed4dba9d59beb4e96b3ce3
edaagunes <edagunes00@hotmail.com>
2024-10-25 15:51:40 +0300

    Completed Admin Panel

--- Project2BurgerMenu/Areas/Admin/Controllers/StatisticsController.cs
+++ Project2BurgerMenu/Areas/Admin/Controllers/StatisticsController.cs
@@ -11,22 +11,21 @@ namespace Project2BurgerMenu.Areas.Admin.Controllers
 {
     public class StatisticsController : Controller
     {
-        BurgerMenuContext context=new BurgerMenuContext();
+        BurgerMenuContext context = new BurgerMenuContext();
         public ActionResult Index()
         {
             ViewBag.products = context.Products.Count();
             ViewBag.reservations = context.Reservations.Count();
             ViewBag.admins = context.Admins.Count();
             ViewBag.testimonials = context.Contacts.Count();
-            ViewBag.deals=context.Products.Where(x=>x.DealofTheDay==true).Select(y=>y.ProductName).Count();
-            string adminEmail = User.Identity.Name;
-            ViewBag.messages=context.Messages.Where(x=>x.ReceiverEmail==adminEmail).Count();
-            ViewBag.categories=context.Categories.Count();
-            ViewBag.mainDishes=context.Products.Where(x => x.CategoryID==1).Count();
-            ViewBag.subscribes=context.Subscribes.Count();  
-            ViewBag.avgPrice=context.Products.Select(x=>x.Price).Average();  
-            ViewBag.maxPrice=context.Products.Select(x=>x.Price).Max();
-            ViewBag.lastReserve = context.Reservations.OrderByDescending(x=>x.ReservationID).Select(x=>x.Time).FirstOrDefault();
+            ViewBag.deals = context.Products.Where(x => x.DealofTheDay == true).Select(y => y.ProductName).Count();
+            ViewBag.canceledReservations = context.Reservations.Where(x => x.ReservationStatus == "İptal Edildi").Count();
+            ViewBag.categories = context.Categories.Count();
+            ViewBag.mainDishes = context.Products.Where(x => x.CategoryID == 1).Count();
+            ViewBag.subscribes = context.Subscribes.Count();
+            ViewBag.avgPrice = context.Products.Select(x => x.Price).Average();
+            ViewBag.maxPrice = context.Products.Select(x => x.Price).Max();
+            ViewBag.lastReserve = context.Reservations.OrderByDescending(x => x.ReservationID).Select(x => x.Time).FirstOrDefault();
             return View();
         }
     }
